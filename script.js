d87be89700779877ae5224b715149fedf892687f
Stephen Leander <stephen.leander1@gmail.com>
2022-05-02 12:41:03 +0200

    add remove button for books

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Create empty library & set html constants 
 let myLibrary = [];
 const tbody = document.querySelector('tbody');
 const body = document.querySelector('body');
 const form = document.createElement('form');
 
+// Create Book object
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,7 +12,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
-
+// Library functions
 function addBookToLibrary(book) {
     myLibrary.push(book);
 };
@@ -21,16 +23,27 @@ function displayBooks(library) {
     for (let book in library) {
         let newBook = document.createElement('tr');
         let newValues = Object.values(library[book]);
+        let newIndex = library.indexOf(library[book]);
+        console.log(newIndex);
         for (value in newValues) {
             let newValue = document.createElement('td');
             newValue.textContent = newValues[value];
             newBook.appendChild(newValue);
         }
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.type = 'button';
+        newBook.appendChild(removeButton);
+        removeButton.addEventListener('click', () => {
+            library.splice(newIndex, 1);
+            displayBooks(library);
+        });
+
         tbody.appendChild(newBook);
     }
 };
 
-
+// Define structure of form
 function generateBookForm() {
     const formTitle = document.createElement('h2');
     formTitle.textContent = 'Book Information';
@@ -68,7 +81,7 @@ function generateBookForm() {
     
     body.appendChild(form);
 
-
+// Add new book to library on button press
     confirmButton.addEventListener('click', () => {
         let newRead = '';
         if (document.getElementById('read').checked) {
@@ -77,31 +90,39 @@ function generateBookForm() {
         let newTitle = document.getElementById('title').value;
         let newAuthor = document.getElementById('author').value;
         let newPages = document.getElementById('pages').value;
-        const newBook = new Book(newTitle, newAuthor, newPages, newRead);
-        addBookToLibrary(newBook);
-        displayBooks(myLibrary);
-        form.innerHTML = '';
+        if (newTitle && newAuthor && newPages) {
+            const newBook = new Book(newTitle, newAuthor, newPages, newRead);
+            addBookToLibrary(newBook);
+            displayBooks(myLibrary);
+            form.innerHTML = '';
+        } else {
+            alert('Please complete the form.');
+        }
     });
 
 };
 
-// Testers
-const theHobbit = new Book('The Hobbit', 'JRR Tolkein', 295, 'not read');
-const warbreaker = new Book('Warbreaker', 'Brandon Sanderson', 1210, 'read');
-addBookToLibrary(warbreaker);
-addBookToLibrary(theHobbit);
-//
-displayBooks(myLibrary);
-
-
+// Create add book form on button press
 const addBookButton = document.createElement('button');
 addBookButton.classList.toggle('new-book');
 addBookButton.textContent = 'Add Book';
 body.appendChild(addBookButton);
-
-
 addBookButton.addEventListener('click', () => {
     if (form.innerHTML == '') {
         generateBookForm();
     };
 });
+
+
+
+
+
+// Testers
+const theHobbit = new Book('The Hobbit', 'JRR Tolkein', 295, '');
+const warbreaker = new Book('Warbreaker', 'Brandon Sanderson', 1210, '✓');
+
+addBookToLibrary(warbreaker);
+addBookToLibrary(theHobbit);
+//
+displayBooks(myLibrary);
+
